bc83fd5d9027e1e9ed7ca83a5d4a8ac79636cddd
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2019-02-06 21:11:59 -0500

    Region and runtime for layers
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- iopipe_install/layers.py
+++ iopipe_install/layers.py
@@ -1,6 +1,6 @@
 import json
 import requests
 
-def list(region):
-    req = requests.get("https://%s.layers.api.iopipe.com/")
+def list(region, runtime):
+    req = requests.get("https://%s.layers.api.iopipe.com/get-layers?CompatibleRuntime=%s" % (region, runtime))
     return json.loads(req.content)
