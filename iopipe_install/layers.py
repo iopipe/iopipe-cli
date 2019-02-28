ba709903c6d3a754ffbc9900fb813f315314d757
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2019-02-28 13:42:03 -0500

    Use the deployed layers api url
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- iopipe_install/layers.py
+++ iopipe_install/layers.py
@@ -2,5 +2,5 @@ import json
 import requests
 
 def list(region, runtime):
-    req = requests.get("https://%s.layers.api.iopipe.com/get-layers?CompatibleRuntime=%s" % (region, runtime))
+    req = requests.get("https://%s.layers.iopipe.com/get-layers?CompatibleRuntime=%s" % (region, runtime))
     return json.loads(req.content).get("Layers", [])
